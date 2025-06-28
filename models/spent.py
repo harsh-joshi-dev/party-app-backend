913009e564b9ecfc86b7fac8d8f723e1838d69b3
Harsh Joshi <Harsh.Joshi@deltacubes.us>
2025-06-28 13:28:35 +0530

    Changes

--- models/spent.py
+++ models/spent.py
@@ -3,21 +3,21 @@ from typing import Optional
 from datetime import date
 
 class SpentCreate(BaseModel):
-    company_id: str
-    type: str
-    amount: float
+    company_id: Optional[str] = None
+    type: Optional[str] = None
+    amount: Optional[float] = None
     note: Optional[str] = None
     bought_date: Optional[date] = None
 
     # Salary-specific fields
-    salary_person: Optional[str]
-    salary_month: Optional[str]  # example: "June 2025"
-    salary_given_date: Optional[date]
-    salary_given_by: Optional[str]
-    salary_payment_type: Optional[str]
+    salary_person: Optional[str] = None
+    salary_month: Optional[str] = None  # example: "June 2025"
+    salary_given_date: Optional[date] = None
+    salary_given_by: Optional[str] = None
+    salary_payment_type: Optional[str] = None
 
     # Expense-specific fields
-    item_name: Optional[str]
-    expense_payment_type: Optional[str]
-    expense_source: Optional[str]
-    expense_source_url_or_site: Optional[str]
+    item_name: Optional[str] = None
+    expense_payment_type: Optional[str] = None
+    expense_source: Optional[str] = None
+    expense_source_url_or_site: Optional[str] = None
