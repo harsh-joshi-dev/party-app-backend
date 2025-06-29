b4fd9ba1bd4b9f8ecfeaf7a2f75bbfe69721a103
Harsh Joshi <Harsh.Joshi@deltacubes.us>
2025-06-29 21:33:01 +0530

    Latest change

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(appointment.router, prefix="/appointments", tags=["Appointments"])
 app.include_router(spent.router, prefix="/spents", tags=["Spents"])
-# app.include_router(payment.router, prefix="/payments", tags=["Payments"])
+app.include_router(payment.router, prefix="/payments", tags=["Payments"])
 
 @app.get("/")
 def read_root():
